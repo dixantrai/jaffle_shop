5e5f1fab18bd147547708e56a40f9eecced99fb0
dixantrai <56809131+dixantrai@users.noreply.github.com>
2019-12-27 15:10:26 -0500

    changing folder structure for macros

--- macros/f_future_date.sql
+++ macros/f_future_date.sql
@@ -1,7 +1,5 @@
 {% macro create_udfs() %}
 
-CREATE SCHEMA IF NOT EXISTS {{target.schema}};
-
 CREATE OR REPLACE FUNCTION {{target.schema}}.array_sort(a array)
   RETURNS array
   LANGUAGE JAVASCRIPT
