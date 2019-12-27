11604cef9993f1b82ad21d11f274b429489e2a24
dixantrai <56809131+dixantrai@users.noreply.github.com>
2019-12-27 14:39:02 -0500

    added test udf array_sort

--- macros/udfs/f_future_date.sql
+++ macros/udfs/f_future_date.sql
@@ -2,10 +2,12 @@
 
 CREATE SCHEMA IF NOT EXISTS {{target.schema}};
 
-CREATE OR REPLACE FUNCTION {{target.schema}}.f_future_date()
-RETURNS TIMESTAMP
-IMMUTABLE AS $$
-SELECT '2100-01-01'::TIMESTAMP;
-$$ LANGUAGE sql
+CREATE OR REPLACE FUNCTION {{target.schema}}.array_sort(a array)
+  RETURNS array
+  LANGUAGE JAVASCRIPT
+AS
+$$
+  return A.sort();
+$$;
 
 {% endmacro %}
